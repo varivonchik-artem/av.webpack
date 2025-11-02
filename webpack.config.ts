a2a82e4aa490bcfb7ab6ed46678dcc82ba190063
Varivonchik Artem <varivonchik.core@gmail.com>
2025-11-02 21:59:53 +0300

    refactor(webpack): decompose configuration into modular components

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,31 +1,21 @@
-import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { buildWebpackConfiguration } from "./configuration/build/buildWebpackConfiguration";
+import { BuildPaths } from "./configuration/types/configurationTypes";
+import path from "path";
 
-module.exports = {
-  mode: "development",
+const PATHS: BuildPaths = {
   entry: path.resolve(__dirname, "src", "index.ts"),
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+  build: path.resolve(__dirname, "build"),
+  html: path.resolve(__dirname, "public", "index.html"),
 };
+
+const MODE = "development";
+const IS_DEVELOPMENT = MODE === "development";
+
+const webpackConfiguration: webpack.Configuration = buildWebpackConfiguration({
+  mode: MODE,
+  paths: PATHS,
+  isDevelopment: IS_DEVELOPMENT,
+});
+
+export default webpackConfiguration;
