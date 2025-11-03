fcfc83d54ee1e8bb4c778602344f105c6596c64a
Varivonchik Artem <varivonchik.core@gmail.com>
2025-11-04 00:24:29 +0300

    feat(styles): integrate MiniCssExtractPlugin and configure css modules

--- configuration/build/buildLoaders.ts
+++ configuration/build/buildLoaders.ts
@@ -1,12 +1,28 @@
 import webpack from "webpack";
+import { BuildOptions } from "../types/configurationTypes";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
-  const cssLoaders = {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDevelopment } = options;
+
+  const cssLoaders: webpack.RuleSetRule = {
     test: /\.s[ac]ss$/i,
-    use: ["style-loader", "css-loader", "sass-loader"],
+    use: [
+      isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resourcePath: string) => /\.module\.\w+$/i.test(resourcePath),
+            localIdentName: isDevelopment ? "[path][name]__[local]" : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
   };
 
-  const typescriptLoaders = {
+  const typescriptLoaders: webpack.RuleSetRule = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
--- configuration/build/buildPlugins.ts
+++ configuration/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 import webpack from "webpack";
 import { BuildOptions } from "../types/configurationTypes";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
   const { paths } = options;
@@ -11,5 +12,9 @@ export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstan
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
   ];
 }
